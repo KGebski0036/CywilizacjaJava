7bb3f1371624f6a4b673a08f82685c40f5428259
112-15\Student <kgebski68@gmail.com>
2022-03-20 09:20:05 +0100

    Adding main Java files

---
+++ Cywilizacja/Cywilizacja/src/test/java/pl/edu/ezse/Cywilizacja/CywilizacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ezse.Cywilizacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CywilizacjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
